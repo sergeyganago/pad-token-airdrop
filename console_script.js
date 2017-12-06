02bdede60593c7043ce21c1eeb8f611dacd6362e
Admin <solarashes>
2017-12-06 15:20:37 -0500

    update files

--- console_script.js
+++
@@ -1,8 +0,0 @@
-PADSale.deployed().then(function(instance){q=instance});
-PADToken.deployed().then(function(instance){t=instance});
-acc = web3.eth.accounts
-
-
-contract = PADSale.at(PADSale.address);
-var event = contract.TokenAddressEvent();
-event.watch(function(err, result){ console.log(result.args) });
